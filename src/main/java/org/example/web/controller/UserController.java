77bdbf7f3d8d3fa1062f3c0b44f75b4199d6a211
Krugerxxx <sssnik@mail.ru>
2022-09-14 12:50:57 +0500

    v 1.0.3

--- src/main/java/org/example/web/controller/UserController.java
+++ src/main/java/org/example/web/controller/UserController.java
@@ -1,11 +1,13 @@
 package org.example.web.controller;
 
+import org.example.web.models.User;
 import org.example.web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/users")
@@ -18,6 +20,42 @@ public class UserController {
     public String welcome(Model model) {
         model.addAttribute("userslist", userService.listAll());
         return "users/users";
+    }
+
+    @RequestMapping(value = "/new_user", method = RequestMethod.GET)
+    public String newUserForm(Model model, User user) {
+        model.addAttribute("newuser", user);
+        return "users/new_user";
+    }
+
+    @RequestMapping(value = "/new_user", method = RequestMethod.POST)
+    public String newUserSubmit(@ModelAttribute @Validated User user, BindingResult result) {
+        if (result.hasErrors()) {
+            return "redirect:/users";
+        }
+        userService.save(user);
+        return "redirect:/users";
+    }
+
+    @RequestMapping(value = "/edit_user{id}", method = RequestMethod.GET)
+    public String editUserForm(@PathVariable(value = "id") Long id, Model model) {
+        model.addAttribute("edituser", userService.getUser(id));
+        return "users/edit_user";
+    }
+
+    @RequestMapping(value = "/edit_user{id}", method = RequestMethod.POST)
+    public String editUser(@ModelAttribute @Validated User user, BindingResult result) {
+        if (result.hasErrors()) {
+            return "redirect:/users";
+        }
+        userService.update(user);
+        return "redirect:/users";
+    }
+
+    @RequestMapping(value = "/delete_user{id}")
+    public String deleteUser(@PathVariable(value = "id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/users";
     }
 
 }

0fc588abe1ac1e18cb2a940559f0c1312373d71e
Krugerxxx <sssnik@mail.ru>
2022-09-17 14:56:38 +0500

    v 1.1.1

--- src/main/java/org/example/web/Application.java
+++ src/main/java/org/example/web/Application.java
@@ -3,10 +3,8 @@ package org.example.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @EntityScan("org.example.web")
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {

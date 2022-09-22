afe7990b50624c23b87c1d0035bdfc0e3914597e
Krugerxxx <sssnik@mail.ru>
2022-09-22 10:13:32 +0500

    v 1.0.4

--- src/main/java/org/example/web/Application.java
+++ src/main/java/org/example/web/Application.java
@@ -2,9 +2,8 @@ package org.example.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan("org.example.web")
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
